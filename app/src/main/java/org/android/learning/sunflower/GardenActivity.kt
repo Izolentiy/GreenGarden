426c20b69346b3c7c542c34e45582371ebae6cb8
Izolentiy <nima.munkonov@gmail.com>
2021-05-09 10:59:32 +0800

    Dependecies version definition moved to root project gradle file. Added Application and Hilt

---
+++ app/src/main/java/org/android/learning/sunflower/GardenActivity.kt
@@ -0,0 +1,6 @@
+package org.android.learning.sunflower
+
+import androidx.appcompat.app.AppCompatActivity
+
+class GardenActivity : AppCompatActivity() {
+}
--- app/src/main/java/org/android/learning/sunflower/SunflowerApplication.kt
+++ app/src/main/java/org/android/learning/sunflower/SunflowerApplication.kt
@@ -1,5 +1,7 @@
 package org.android.learning.sunflower
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class SunflowerApplication() : Application()
